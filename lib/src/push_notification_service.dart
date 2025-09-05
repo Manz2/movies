cceca0dfb7b7fb7a7bd0f4ce54168749447c3fca
Manz2 <erikmanzer21@gmail.com>
2025-09-05 19:53:20 +0200

    #44 feat: remove unused import of flutter/services.dart from PushNotificationService

--- lib/src/push_notification_service.dart
+++ lib/src/push_notification_service.dart
@@ -4,7 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:flutter/services.dart';
 import 'package:logger/logger.dart';
 import 'package:movies/main.dart';
 import 'package:movies/src/home/movie.dart';
